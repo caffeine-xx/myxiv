1f532541a6f856f0aadf364005dcebbba8f7edf3
Daniel Cook <dan@tanto>
2011-01-04 23:45:01 +0000

    search parser

--- search_parser.py
+++ search_parser.py
@@ -1,7 +1,9 @@
+import datetime
 from lepl import *
 
+
 def node(x):
-    f = lambda y: {x:y}
+    f = lambda y: (x,y)
     return f
 
 def comp(*fs):
@@ -25,6 +27,40 @@ class search_parser:
     atom     = tag | year | contains | keyword 
     with DroppedSpace():
         expr = atom[:]
+    """ Parser that builds queries """
+    def __init__(self):
+        pass
+    def parse(self,s):
+        return self.expr.parse(s)
+
+class query_builder:
+    """ Builds query dicts for mongoengine's Article collection 
+        Allows chaining style like 
+            x.tag("foo").year(1998)""" 
+    def __init__(self):
+        self.q = {}
+    def exists(self,v):
+        self.q.update({v:{'$exists':True}})
+        return self
+    def tag(self,v):
+        return self.exists('tags__'+v)
+    def keyword(self,v):
+        return self.exists('description__ngram_prob__1__'+v)
+    def year(self,v):
+        k = 'published_on__gt'
+        if not self.q.has_key(k) or (self.q.has_key(k) and v > self.q[k]):
+            self.q['published_on__gt']=datetime.datetime(int(v),1,1)
+        return self
+    def get(self):
+        return self.q
 
 def parse(s):
-    return search_parser.expr.parse(s)
+    return search_parser().parse(s)
+
+def parse_query(s):
+    query = parse(s)
+    qb = query_builder()
+    for (k,v) in query:
+        getattr(qb,k)(v)
+    return qb.get()
+
--- test_search_parser.py
+++ test_search_parser.py
@@ -1,20 +1,52 @@
-import search_parser, time
+import search_parser, time, datetime
 reload(search_parser)
 
 def test_parse():
-    tests = {'foo':[{'keyword':'foo'}],
-             'foo bar':[{'keyword':'foo'},{'keyword':'bar'}],
-             'y:2000':[{'year':2000}],
-             't:tag':[{'tag':'tag'}],
-             '"zig zag"':[{'keyword':"zig zag"}]}
+    kw = search_parser.node('keyword')
+    yr = lambda x: search_parser.node('year')(int(x))
+    tg = search_parser.node('tag')
+
+    tests = {'foo':[kw('foo')],
+             'foo bar':[kw('foo'),kw('bar')],
+             'y:2000':[yr(2000)],
+             't:tag':[tg('tag')],
+             '"zig zag"':[kw("zig zag")]}
     # Ensure results are correct
     for s,r in tests.items():
         result = search_parser.parse(s)
         assert result==r, "Input %s, expected %s, got %s" % (s, str(r),str(result))
     t0 = time.time()
-    N  = 10
+    N  = 20
     for x in xrange(N):
         map(search_parser.parse, tests.keys())
     dt = time.time()-t0
     print "Parses / sec: %0.2f" % (len(tests)*N/dt)
 
+def test_query_builder():
+    qb = search_parser.query_builder()
+    ex = {"$exists":True}
+    tests = [
+        (lambda x: x.tag("tag1"),
+            {"tags__tag1":ex}),
+        (lambda x: x.tag("tag1").tag("tag2"),
+            {"tags__tag1":ex,
+             "tags__tag2":ex}),
+        (lambda x:x.keyword("key1"),
+            {'description__ngram_prob__1__key1':ex}),
+        (lambda x:x.year("1998"),
+            {"published_on__gt":datetime.datetime(1998,1,1)}),
+        (lambda x:x.tag("tag1").tag("tag2").keyword('key1').keyword('key2').year('1998'),
+            {"published_on__gt":datetime.datetime(1998,1,1),
+             'description__ngram_prob__1__key1':ex,
+             'description__ngram_prob__1__key2':ex,
+             "tags__tag1":ex,
+             "tags__tag2":ex})]
+    for (f,v) in tests:
+        q = f(search_parser.query_builder()).get()
+        assert v==q, "Expected: %s, got %s" % (str(v),str(q))
+
+def test_parse_query():
+    qb = search_parser.query_builder()
+    q1 = qb.tag("foo").keyword("bar").get()
+    q2 = search_parser.parse_query("t:foo bar")
+    assert q1==q2, "Expected: %s, got %s" % (str(v),str(q))
